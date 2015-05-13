b7991e9bc4db64b31569fe2f58a2ac9a99a5c548
joantas dias <jonatasbh32@yahoo.com.br>
2015-05-13 14:18:39 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include_once "topo.php"; ?>
+<?php include_once "logicaFooter.php"; ?>
+
+		<div id="conteudo">
+			Conteúdo do site
+		</div>
+
+		<hr />
+
+		<div id="footer">
+			<p>Você acessou a pagina - <?php echo $home; ?></p>
+		</div>
+		
+	</div>
+
+</body>
+</html>

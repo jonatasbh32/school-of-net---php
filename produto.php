3bf21fc167290bc4786de458817872e275e806f6
joantas dias <jonatasbh32@yahoo.com.br>
2015-05-13 14:19:59 -0300

    Create produto.php

---
+++ produto.php
@@ -0,0 +1,19 @@
+<?php include_once "topo.php"; ?>
+
+<?php include_once "logicaFooter.php"; ?>
+
+		<div id="conteudo">
+			produtos - conteúdo 
+		</div>
+
+
+		<hr />
+
+		<div id="footer">
+			<p>Você acessou a pagina - <?php echo $produtos; ?></p>
+		</div>
+		
+	</div>
+
+</body>
+</html>

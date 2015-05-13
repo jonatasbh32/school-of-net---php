5b4e86cea7a2fd1be84e220163f41a4983f9d5ce
joantas dias <jonatasbh32@yahoo.com.br>
2015-05-13 14:19:04 -0300

    Create localizacao.php

---
+++ localizacao.php
@@ -0,0 +1,18 @@
+<?php include_once "topo.php"; ?>
+<?php include_once "logicaFooter.php"; ?>
+
+		<div id="conteudo">
+			Localização - Conteúdo 
+		</div>
+
+
+		<hr />
+
+		<div id="footer">
+			<p>Você acessou a pagina - <?php echo $localizacao; ?></p>
+		</div>
+		
+	</div>
+
+</body>
+</html>

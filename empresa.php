158fdfb23fb8a0ac8fa487f8915cc5a17036db9f
joantas dias <jonatasbh32@yahoo.com.br>
2015-05-13 14:18:19 -0300

    Create empresa.php

---
+++ empresa.php
@@ -0,0 +1,14 @@
+?php include_once "topo.php"; ?>
+<?php include_once "logicaFooter.php"; ?>
+
+		<div id="conteudo">
+			Empresa - conteúdo
+		</div>
+
+	<hr />
+
+		<div id="footer">
+			<p>Você acessou a pagina - <?php echo $empresa; ?></p>
+		</div>
+		
+	</div>

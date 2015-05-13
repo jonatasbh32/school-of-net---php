3619eadf408716806b082b9392db27ce6c68d8bd
joantas dias <jonatasbh32@yahoo.com.br>
2015-05-13 14:17:49 -0300

    Create contato.php

---
+++ contato.php
@@ -0,0 +1,18 @@
+<?php include_once "topo.php"; ?>
+<?php include_once "logicaFooter.php"; ?>
+
+		<div id="conteudo">
+			Contato - Conteúdo
+		</div>
+
+
+		<hr />
+
+		<div id="footer">
+			<p>Você acessou a pagina - <?php echo $contato; ?></p>
+		</div>
+		
+	</div>
+
+</body>
+</html>
